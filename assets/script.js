f4add73b0623fd84022de6022fd8f0a09f8bb3b5
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-06-08 19:57:18 +0100

    update

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+"use strict";
+const day = document.querySelector(".day");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+
+const countDownDate = new Date("June 8, 2024 00:00:00");
+
+const countDownTime = function () {
+  const count = setInterval(() => {
+    const now = new Date().getTime();
+    let timeLeft = countDownDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    const mins = Math.floor((timeLeft / 1000 / 60) % 60);
+    const secs = Math.floor((timeLeft / 1000) % 60);
+
+    day.textContent = +days;
+    hour.textContent = +hours;
+    minute.textContent = +mins;
+    seconds.textContent = +secs;
+
+    const lasttime = 0;
+
+    if (timeLeft < 0) {
+      clearInterval(count);
+      day.textContent = lasttime;
+      hour.textContent = lasttime;
+      minute.textContent = lasttime;
+      seconds.textContent = lasttime;
+    }
+  }, 1000);
+};
+
+countDownTime();
